f8036ca0c9933f998d6cc3f1cc7d2eccfcd42c4c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-24 22:12:32 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
-import { Redirect, Route } from 'react-router-dom'
-import Home from './pages/Home'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -28,12 +26,8 @@ const App = () => (
 	<IonApp>
 		<IonReactRouter>
 			<IonRouterOutlet>
-				<Route exact path="/home">
-					<Home />
-				</Route>
-				<Route exact path="/">
-					<Redirect to="/home" />
-				</Route>
+			
+	
 			</IonRouterOutlet>
 		</IonReactRouter>
 	</IonApp>
