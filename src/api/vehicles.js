d7e228fb40051ebfacb8151fd62f445c69d05fc8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-27 15:38:24 +0300

    update filter

--- src/api/vehicles.js
+++ src/api/vehicles.js
@@ -1,17 +1,33 @@
 import Vehicle from './vehicle'
 
+const filterByStringKeys = ['brand', 'fuel', 'gear']
+const filterByNumberKeys = ['minAge', 'minLicense']
+const sortKeys = ['hourPrice', 'dayPrice', 'kmPrice']
+
 const mockVehicles = [new Vehicle('https://s3.eu-west-1.amazonaws.com/files.driveyoyo.com/yoyoircom/media/files/23000/84/2023/8/20230804100527937-50959-23-6921.png', 'Renault Clio', 'Hatchback', 'Otomatik', 'Benzin', 179, 1199, 4.99, 750, 2015, 21, 1), new Vehicle('https://s3.eu-west-1.amazonaws.com/files.driveyoyo.com/yoyoircom/media/files/23000/95/2023/8/20230811175832083-67659-19-4293.png', 'Volkswagen Polo', 'Hatchback', 'Otomatik', 'Benzin', 219.0, 1499.0, 4.99, 750, 2015, 21, 1), new Vehicle('https://s3.eu-west-1.amazonaws.com/files.driveyoyo.com/yoyoircom/media/files/23000/68/2023/7/20230719120005116-46386-18-626.png', 'Fiat Egea Cross', 'Hatchback', 'Otomatik', 'Dizel', 219.0, 1499.0, 4.79, 900, 2015, 22, 3), new Vehicle('https://s3.eu-west-1.amazonaws.com/files.driveyoyo.com/yoyoircom/media/files/23000/90/2023/7/20230719120732278-51616-13-5210.jpg', 'Ford Kuga', 'Hatchback', 'Otomatik', 'Benzin', 359.0, 2399.0, 5.49, 3000, 2015, 25, 5)]
 
 export const getVehicles = filters => {
-	const vehicles = [...mockVehicles]
+	let vehicles = [...mockVehicles]
 
 	for (let [key, val] of Object.entries(filters)) {
-		console.log('Key: ', key)
-		console.log('Val: ', val)
+		if (filterByStringKeys.includes(key)) {
+			vehicles = vehicles.filter(v => v[key] === val)
+		} else if (filterByNumberKeys.includes(key)) {
+			vehicles = vehicles.filter(v => v[key] >= val)
+		} else if (sortKeys.includes(key)) {
+			if (val == 'asc') {
+				vehicles.sort((a, b) => a[key] - b[key])
+			} else {
+				vehicles.sort((a, b) => b[key] - a[key])
+			}
+		}
 	}
+
+	return vehicles
 }
 
 export const brands = ['Renault Clio', 'Volkswagen Polo', 'Fiat Egea Cross', 'Ford Kuga']
+export const types = ['Sedan', 'Hatchback']
 export const fuels = ['Benzin', 'Dizel']
 export const gear = ['Otomatik', 'Manuel']
 export const year = [1, 2, 3, 4, 5]
---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,155 @@
+import { IonButton, IonButtons, IonContent, IonHeader, IonItem, IonList, IonModal, IonSelect, IonSelectOption, IonToolbar } from '@ionic/react'
+import { useState } from 'react'
+import { getVehicles, year } from '../../api/vehicles'
+
+const Filter = ({ isFilterOpen, setIsFilterOpen, setVehicles }) => {
+	const [filters, setFilters] = useState({})
+
+	return (
+		<IonModal
+			isOpen={isFilterOpen}
+			onDidDismiss={() => {
+				setIsFilterOpen(false)
+				setFilters({})
+			}}
+		>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot="start">
+						<IonButton onClick={() => setIsFilterOpen(false)}>İptal</IonButton>
+					</IonButtons>
+					<IonButtons slot="end">
+						<IonButton
+							onClick={() => {
+								console.log(filters)
+								setVehicles(getVehicles(filters))
+								setIsFilterOpen(false)
+							}}
+						>
+							Filtrele
+						</IonButton>
+					</IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent scrollY={false}>
+				<IonList lines="full">
+					<IonItem>
+						<IonSelect
+							label="Marka"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, brand: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'Renault Clio'}>Renault Clio</IonSelectOption>
+							<IonSelectOption value={'Dizel'}>Dizel</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Yakıt"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, fuel: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'Benzin'}>Benzin</IonSelectOption>
+							<IonSelectOption value={'Dizel'}>Dizel</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Tür"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, type: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'Sedan'}>Sedan</IonSelectOption>
+							<IonSelectOption value={'Hatchback'}>Hatchback</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Vites"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, gear: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'Otomatik'}>Otomatik</IonSelectOption>
+							<IonSelectOption value={'Manuel'}>Manuel</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Yaş"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, minAge: e.detail.value })
+							}}
+						>
+							{year.map((age, i) => (
+								<IonSelectOption key={i} value={age}>
+									{age}
+								</IonSelectOption>
+							))}
+						</IonSelect>
+					</IonItem>
+
+					<IonItem>
+						<IonSelect
+							label="Ehliyet Yaşı"
+							labelPlacement="fixed"
+							onIonChange={e => {
+								setFilters({ ...filters, minLicense: e.detail.value })
+							}}
+						>
+							{year.map((age, i) => (
+								<IonSelectOption key={i} value={age}>
+									{age}
+								</IonSelectOption>
+							))}
+						</IonSelect>
+					</IonItem>
+
+					<IonItem>
+						<IonSelect
+							label="Saatlik"
+							onIonChange={e => {
+								setFilters({ ...filters, hourPrice: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'asc'}>Artan Fiyat</IonSelectOption>
+							<IonSelectOption value={'desc'}>Azalan Fiyat</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Günlük"
+							onIonChange={e => {
+								setFilters({ ...filters, dayPrice: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'asc'}>Artan Fiyat</IonSelectOption>
+							<IonSelectOption value={'desc'}>Azalan Fiyat</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonSelect
+							label="Kilometre Başı"
+							onIonChange={e => {
+								setFilters({ ...filters, kmPrice: e.detail.value })
+							}}
+						>
+							<IonSelectOption value={'asc'}>Artan Fiyat</IonSelectOption>
+							<IonSelectOption value={'desc'}>Azalan Fiyat</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+				</IonList>
+			</IonContent>
+		</IonModal>
+	)
+}
+
+export default Filter
